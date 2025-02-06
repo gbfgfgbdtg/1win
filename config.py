98389ab6719aa213f1eef4536cb1cbf997cd9c55
gbfgfgbdtg <gvefvfverf@gmail.com>
2025-02-06 06:56:29 -1000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+﻿BOT_TOKEN = "7704697508:AAHg_nGXsbYyDYmkqW5vSg65yOKrR0aryks"  # Токен бота
+CHANNEL_URL = "https://t.me/+E9RZrtZtAF01Mzky"  # Ссылка на канал (проверка сабки)
+CHANNEL_ID = "-1002412102944"  # ID канала. Начинается с минуса (проверка сабки)
+VERIF_CHANNEL_ID = "-1002325993274"  # ID обсуждений канала для верификации айдишников, начинается с минуса
+SUPP = "https://t.me/aergrdf"
+ADMIN_ID = 1298501842 #свой айди
+
